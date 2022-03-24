2997de8d0ef2084241b1c94201e8845a1bc898d2
Marijn Haverbeke <marijn@haverbeke.nl>
2022-03-24 11:06:44 +0100

    Move highlighting information into @lezer/markdown

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,36 +1,11 @@
 import {Language, defineLanguageFacet, languageDataProp, foldNodeProp, indentNodeProp,
         LanguageDescription, ParseContext} from "@codemirror/language"
-import {styleTags, tags as t} from "@codemirror/highlight"
 import {parser as baseParser, MarkdownParser, GFM, Subscript, Superscript, Emoji} from "@lezer/markdown"
 
 const data = defineLanguageFacet({block: {open: "<!--", close: "-->"}})
 
 const commonmark = baseParser.configure({
   props: [
-    styleTags({
-      "Blockquote/...": t.quote,
-      HorizontalRule: t.contentSeparator,
-      "ATXHeading1/... SetextHeading1/...": t.heading1,
-      "ATXHeading2/... SetextHeading2/...": t.heading2,
-      "ATXHeading3/...": t.heading3,
-      "ATXHeading4/...": t.heading4,
-      "ATXHeading5/...": t.heading5,
-      "ATXHeading6/...": t.heading6,
-      "Comment CommentBlock": t.comment,
-      Escape: t.escape,
-      Entity: t.character,
-      "Emphasis/...": t.emphasis,
-      "StrongEmphasis/...": t.strong,
-      "Link/... Image/...": t.link,
-      "OrderedList/... BulletList/...": t.list,
-      "BlockQuote/...": t.quote,
-      "InlineCode CodeText": t.monospace,
-      URL: t.url,
-      "HeaderMark HardBreak QuoteMark ListMark LinkMark EmphasisMark CodeMark": t.processingInstruction,
-      "CodeInfo LinkLabel": t.labelName,
-      LinkTitle: t.string,
-      Paragraph: t.content
-    }),
     foldNodeProp.add(type => {
       if (!type.is("Block") || type.is("Document")) return undefined
       return (tree, state) => ({from: state.doc.lineAt(tree.from).to, to: tree.to})
@@ -51,20 +26,7 @@ export function mkLang(parser: MarkdownParser) {
 /// Language support for strict CommonMark.
 export const commonmarkLanguage = mkLang(commonmark)
 
-const extended = commonmark.configure([GFM, Subscript, Superscript, Emoji, {
-  props: [
-    styleTags({
-      "TableDelimiter SubscriptMark SuperscriptMark StrikethroughMark": t.processingInstruction,
-      "TableHeader/...": t.heading,
-      "Strikethrough/...": t.strikethrough,
-      TaskMarker: t.atom,
-      Task: t.list,
-      Emoji: t.character,
-      "Subscript Superscript": t.special(t.content),
-      TableCell: t.content
-    })
-  ]
-}])
+const extended = commonmark.configure([GFM, Subscript, Superscript, Emoji])
 
 /// Language support for [GFM](https://github.github.com/gfm/) plus
 /// subscript, superscript, and emoji syntax.
